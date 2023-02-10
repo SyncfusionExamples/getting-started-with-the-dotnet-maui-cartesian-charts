63f7c8bbbc5386e2efe4369948398d7a8213a037
vinoth-Kumar-S <31274282+vinoth-Kumar-S@users.noreply.github.com>
2023-02-10 19:26:35 +0530

    Adding MAUI Cartesian Charts video
    Adding MAUI Cartesian Charts video

---
+++ MauiProject/DataViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiProject
+{
+    public class DataViewModel
+    {
+        public List<Person> Data { get; set; }
+        public DataViewModel() {
+            Data = new List<Person>()
+            {
+                new Person { Name = "David", Height = 170 },
+                new Person { Name = "Michael", Height = 96 },
+                new Person { Name = "Steve", Height = 65 },
+                new Person { Name = "Joel", Height = 182 },
+                new Person { Name = "Bob", Height = 134 }
+            };
+        }
+    }
+}
