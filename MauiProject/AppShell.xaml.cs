eb84d361db888f30d8a9728afa94219787482bc4
KompelliSravanSyncfusion <kompellisravankumark@syncfusion.com>
2025-04-29 09:47:16 +0530

    Updated the sample with latest version Net 9

--- MauiProject/AppShell.xaml.cs
+++ MauiProject/AppShell.xaml.cs
@@ -1,9 +1,10 @@
-﻿namespace MauiProject;
-
-public partial class AppShell : Shell
+﻿namespace MauiProject
 {
-	public AppShell()
-	{
-		InitializeComponent();
-	}
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
 }
